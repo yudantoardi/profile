f5b89b8dc9a05b8dd221406a82dcaa10eed57c39
yudantoardi <yudantoardi@gmail.com>
2021-09-02 15:27:24 +0700

    update template

---
+++ asset/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.main-nav li a[href*="#"]').click(function (event) {
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 750);
+            }
+        }
+    });
+});
